ae4a41cab842f9c38769f3e59ad13e4fabf8ac13
gabrielkulp <kulpga@oregonstate.edu>
2022-05-27 15:40:36 -0700

    add statistics gathering to server

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from logging import exception
 import sys
 import socket
 import asyncio
@@ -21,24 +22,26 @@ class ResponderServer:
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind(("0.0.0.0", PORT_SIG))
-        print("sending share request")
+        #print("sending share request")
         sock.sendto(b'\xff', INITIALIZER_ADDR)
         (data, _) = sock.recvfrom(1024)
         self.share = go.deserialize(data)
         sock.close()
-        print("got my share:", self.share)
+        #print("got my share:", self.share)
 
     def connection_made(self, transport):
         self.transport = transport
 
     def datagram_received(self, data, addr):
+        if data == b"\xff":
+            exit(0)
         idx = data[0]
         m = data[1:]
         psign = self.bls.sign(self.share, m)
         res = idx.to_bytes(1, "big") + self.go.serialize(psign)
         #time.sleep(.1)
         self.transport.sendto(res, INITIALIZER_ADDR)
-        print("sent signature for", idx)
+        #print("sent signature for", idx)
 
 
 class InitiatorServer:
@@ -59,6 +62,10 @@ class InitiatorServer:
         self.signs = []
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+
+        global sig_count
+        sig_count = 0
+
         self.initiate_new()  # kickstart the whole process
 
     def connection_made(self, transport):
@@ -70,16 +77,16 @@ class InitiatorServer:
 
         if data == b'\xff':
             # this is a request for a share
-            print("got share request")
+            print("got share request from", res_idx)
             res = self.go.serialize(self.all_shares[res_idx])
             self.transport.sendto(res, (ip, PORT_SIG))
-            print("sent share:", self.all_shares[res_idx])
+            #print("sent share:", self.all_shares[res_idx])
             self.initiate_new()
             return
 
         if data == b'\xfe':
             # this is a request to start over
-            print("initiating new")
+            #print("initiating new")
             self.initiate_new()
             return
 
@@ -88,20 +95,21 @@ class InitiatorServer:
         share = self.go.deserialize(data[1:])
 
         if seq == self.seq:
-            print(f"got signature {seq} from {res_idx}")
+            #print(f"got signature {seq} from {res_idx}")
             self.signs.append((res_idx+1, share))
-        else:
+            if len(self.signs) >= self.t:
+                self.aggregate_and_verify()
+                self.initiate_new()
+        #else:
             #print(f"sequence number mismatch. Expected {self.seq} got {seq} from {res_idx}")
-            print("discarding extra share")
-
-        if len(self.signs) >= self.t:
-            self.aggregate_and_verify()
-            self.initiate_new()
+            #print("discarding extra share")
 
     def aggregate_and_verify(self):
         sig = self.bls.aggregate(self.signs)
-        print(f"Message: '{self.m}'")
-        print(f"Signature: {sig}")
+        global sig_count
+        sig_count += 1
+        #print(f"Message: '{self.m}'")
+        #print(f"Signature: {sig}")
         #if self.bls.verify(self.pk, sig, self.m):
         #    print("Valid signature!")
         #else:
@@ -118,12 +126,11 @@ class InitiatorServer:
         self.signs = []
         #time.sleep(.1)
         msg = self.seq.to_bytes(1, "big") + self.m
-        print("sending request for", self.seq)
+        #print("sending request for", self.seq)
         self.sock.sendto(msg, (MCAST_SIGN, PORT_SIGN))
 
 
 def main():
-    server = None
     groupObj = PairingGroup('MNT224')
     bls = BLSTHS(groupObj)
 
@@ -148,26 +155,41 @@ def main():
         print("Starting responder")
         time.sleep(2)
 
+        loop.run_until_complete(server)
+
+        print("starting loop")
+        loop.run_forever()
+
     else:  # initiator
-        if len(sys.argv) != 3:
-            print("Must specify number and threshold")
+        if len(sys.argv) != 4:
+            print("Must specify number, threshold, and test length")
             exit(1)
         n = int(sys.argv[1])
         t = int(sys.argv[2])
+        delay = float(sys.argv[3])
 
         (pk, shares) = bls.keygen(n, t)
         server = loop.create_datagram_endpoint(
             lambda: InitiatorServer(groupObj, bls, shares, n, t, pk, messages),
             local_addr=('0.0.0.0', PORT_INIT))
         print("starting initiator")
+        loop.run_until_complete(server)
 
-    loop.run_until_complete(server)
+        print("starting loop")
+        try:
+            loop.run_until_complete(asyncio.sleep(delay))
+        except TimeoutError:
+            print("done")
+        global sig_count
+        print(f"Completed {sig_count} in {round(delay,2)} seconds.")
+        print(f"Average is {round(sig_count/(delay),2)} signatures per second")
 
-    print("starting loop")
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
+        # ask responders to die
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+        sock.sendto(b"\xff", (MCAST_SIGN, PORT_SIGN))
+        
+
 
 
 if __name__ == "__main__":
